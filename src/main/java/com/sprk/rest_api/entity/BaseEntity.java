15513e8024f2c0ff685b6ecd02872cdab17e6d4a
Gani25 <memonabdulgani3d@gmail.com>
2024-09-27 19:55:47 +0530

    Added Save Employee API wuth audit aware

--- src/main/java/com/sprk/rest_api/entity/BaseEntity.java
+++ src/main/java/com/sprk/rest_api/entity/BaseEntity.java
@@ -1,16 +1,19 @@
 package com.sprk.rest_api.entity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Data
 public class BaseEntity {
 
