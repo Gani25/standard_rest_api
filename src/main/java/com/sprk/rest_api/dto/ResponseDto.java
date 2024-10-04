619829411161c837f844b1abbcdacd32ad901fbc
Gani25 <memonabdulgani3d@gmail.com>
2024-10-04 20:50:10 +0530

    Update ResponseDto.java

--- src/main/java/com/sprk/rest_api/dto/ResponseDto.java
+++ src/main/java/com/sprk/rest_api/dto/ResponseDto.java
@@ -1,16 +1,23 @@
 package com.sprk.rest_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        name = "Response",
+        description = "Schema to Hold Response"
+)
 public class ResponseDto<T> {
 
+    @Schema(description = "Status Code" , example = "200")
     private String statusCode;
 
 //    private String statusMessage;
 
+    @Schema(description = "Data" , example = "Update Successfull")
     private T data;
 
 
